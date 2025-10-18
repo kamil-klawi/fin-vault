1ea2d73cdf3f622cad56f6be85261812a782c114
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-10-18 15:39:50 +0200

    feat(security): add method to check if a permission has attribute

---
+++ apps/SharedLibrary/SharedLibrary/Common/Security/HasPermissionAttribute.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace SharedLibrary.Common.Security
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    public class HasPermissionAttribute : AuthorizeAttribute
+    {
+        public HasPermissionAttribute(string permission)
+        {
+            Policy = $"Permission:{permission}";
+        }
+    }
+}
